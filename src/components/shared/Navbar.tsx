72c402ea97fde2f9652ab05d08d860b86bb4b557
katawutp <katawut.paramee@gmail.com.com>
2025-08-13 20:01:49 +0700

    Navbar with Services

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -48,6 +48,7 @@ const links: NavLink[] = [
   { name: "Paintings", href: "/paintings" },
   { name: "Photographs", href: "/photographs" },
   { name: "Crafts", href: "/crafts" },
+  { name: "Services", href: "/services" },
   { name: "Trade", href: "/collection/137/0x5F53924aE42b8DaC8D5605423a5B432c627A8365/" },
 ];
 
